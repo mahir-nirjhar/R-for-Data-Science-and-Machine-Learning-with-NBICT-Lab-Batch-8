1675cb5d2c69fa4947a01586246dffcf7de19f29
mahir-nirjhar <nirjharmahir@gmail.com>
2025-06-18 23:00:22 +0600

    histogram-02

--- RDSML-Day-14 (Histogram in R (Part-03)/histogram-02.R
+++ RDSML-Day-14 (Histogram in R (Part-03)/histogram-02.R
@@ -23,10 +23,26 @@ ggplot(dataset, aes(x = weight, fill = sex)) +
   theme(legend.position = "top")
 
 # The Plyr package is used to calculate the mean weight of each group.
- install.packages("Plyr")
+install.packages("Plyr")
+library(plyr)
+group_means = ddply(dataset, "sex", summarise, grp.mean = mean(weight)) 
+
+# Adding mean lines
+myPlot = ggplot(dataset, aes(x = weight, fill = sex)) +
+  geom_histogram(color = "white", alpha = .5, position = "dodge") +
+  geom_histogram(color = "white", alpha = 1, position = "dodge") +
+  geom_vline(data = group_means, aes(xintercept = grp.mean, color = sex), 
+             linetype = "dashed") +
+  theme(legend.position = "top")
+
+myPlot
+
+# Using custom color palettes
+myPlot + scale_color_manual(values = c("#999999", "#E69F00")) +
+  scale_fill_manual(values = c("#999999", "#E69F00"))
 
 
-
-
-
+# Using color brewer color palettes
+myPlot + scale_color_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2")
 
