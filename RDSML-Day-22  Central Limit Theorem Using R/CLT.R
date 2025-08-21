108fea2a1eb8b9434009117e78564834f1eb12d5
mahir-nirjhar <nirjharmahir@gmail.com>
2025-08-22 02:01:37 +0600

    CLT.R

---
+++ RDSML-Day-22  Central Limit Theorem Using R/CLT.R
@@ -0,0 +1,33 @@
+# Demonstration of CLT
+
+# Generate 1000 numbers of uniform distribution
+r <- runif(10000)
+hist(r)
+meanr <- mean(r)
+sdr <- sd(r)
+
+# Draw three random samples from the distribution
+samples1 <- sample(r, 4)
+samples2 <- sample(r, 4)
+samples3 <- sample(r, 4)
+
+mean(samples1)
+mean(samples2)
+mean(samples3)
+
+# Run a for loop
+cl4 <- mean(sample(r, 4))
+
+for (i in 1: 1000){
+  cl4 <- c(cl4, mean(sample(r, 4)))
+}
+
+hist(cl4)
+
+cl100 <- mean(sample(r, 100))
+
+for (i in 1: 1000){
+  cl100 <- c(cl100, mean(sample(r, 100)))
+}
+
+hist(cl100)
